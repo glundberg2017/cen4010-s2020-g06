53f52b9f8d48a510ba163af88156309aebff6538
Grant Lundberg <grant@Grants-MacBook-Air.local>
2020-07-28 21:14:01 -0400

    add on_sendButton_clicked

--- covid_communicator/chat_window.py
+++ covid_communicator/chat_window.py
@@ -6,3 +6,6 @@ class ChatWindow(QtWidgets.QDialog):
     def __init__(self):
         super(ChatWindow, self).__init__()
         uic.loadUi('./chat_window.ui', self)
+
+    def on_sendButton_clicked(self):
+        print('sending message to database: {}'.format(self.messageLineEdit.text()))
